10c3b8f207cdf6f08b914e9f4159a4498f602937
Felipe Martins <felipe.martins@leucotron.com.br>
2019-11-21 15:02:33 -0300

    Navigation graph created.

---
+++ app/src/main/java/com/felipe/androidportfolio/fragment/MainFragment.kt
@@ -0,0 +1,39 @@
+package com.felipe.androidportfolio.fragment
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+
+import com.felipe.androidportfolio.R
+import kotlinx.android.synthetic.main.fragment_main.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MainFragment : Fragment(), View.OnClickListener {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        btnStart.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            btnStart.id -> {
+                findNavController().navigate(R.id.portfolioListFragment)
+            }
+        }
+    }
+}
